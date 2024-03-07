e116d47f724fca891949a7b23de8a7b5ed2b2665
AnOrdinaryUsername <57053268+AnOrdinaryUsername@users.noreply.github.com>
2024-03-07 14:28:26 -0800

    Utilize more images for testing predictions

--- apps/model/src/predict.py
+++ apps/model/src/predict.py
@@ -6,16 +6,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 home = os.environ['PROJECT_DIR']
+images = ['osprey', 'owl', 'annas', 'mallard', 'pelican']
 
-# Load a pretrained YOLO model
-model = YOLO(f'{home}/runs/train/weights/best.pt')
+for bird in images:
+    if not os.path.isfile(f"{home}/examples/{bird}.jpg"):
+        raise FileNotFoundError(f"File '{home}/examples/{bird}.jpg' does not exist.")
+
+model = YOLO(f'{home}/runs/train2/weights/best.pt')
 
 # Perform object detection on an image using the model
-results = model('./examples/owl.jpg')
+bird_images = [f'{home}/examples/{bird}.jpg' for bird in images]
+results = model.predict(bird_images)
 
-for result in results:
+for i in range(len(images)):
+    result = results[i]
+
     data = json.loads(result.tojson())
     print(json.dumps(data, indent=2))
-    result.save(filename='./examples/result.jpg')
 
-print("Result file saved")
+    result.save(filename=f'./examples/{images[i]}_result.jpg')
+
+print("\nCompleted predictions")
