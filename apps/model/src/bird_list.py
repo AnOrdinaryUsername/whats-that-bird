73a67bf7fb088fea9d8995cc0d2c24d5a21db5c4
AnOrdinaryUsername <57053268+AnOrdinaryUsername@users.noreply.github.com>
2024-03-14 23:50:28 -0700

    Get all bird names from CRBC
    https://californiabirds.org/checklist.asp
    
    Instead of hard coding the bird names, parse the HTML
    document and write to a csv file for viewing.
    
    We will now train for almost every single bird in California
    since automation makes it fairly easy.
    
    Use excluded_species.txt to document birds excluded from training
    due to not enough flickr data or very poor quality data.

---
+++ apps/model/src/bird_list.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+# List of all Californian birds from the California Bird Records Committee
+url = "https://californiabirds.org/checklist.asp"
+
+# Send a GET request to the website
+response = requests.get(url)
+
+# Parse the HTML content of the website
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Extract all <p> tags with class 'species'
+bird_species = soup.find_all("p", class_="species")
+
+birds = []
+
+for bird in bird_species:
+    name = [text for text in bird.stripped_strings][0]
+
+    print("Bird species:", name)
+    birds.append(name)
+
+# Check to make sure the species count on the website matches the list length
+print(f"\nAcquired names of {len(birds)} bird species in California")
+
+# Save all bird species to csv file
+with open("california_birds.csv", mode="w+") as file:
+    # Create a CSV writer object
+    writer = csv.writer(file)
+
+    # Write each string from the list as a row in the CSV file
+    for bird in birds:
+        writer.writerow([bird])
+
+print("\nSaved all species to 'california_birds.csv'")
