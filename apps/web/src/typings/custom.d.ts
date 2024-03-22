074f98d1fa5439ee2681aee865661528e5b61c46
AnOrdinaryUsername <57053268+AnOrdinaryUsername@users.noreply.github.com>
2024-03-21 22:03:37 -0700

    Fix cannot find type declarations for css modules

--- apps/web/src/typings/custom.d.ts
+++ apps/web/src/typings/custom.d.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line react/no-typos` 
 import 'react';
 
+declare module "*.module.css";
+
 declare module 'react' {
   interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
     jsx?: boolean;
